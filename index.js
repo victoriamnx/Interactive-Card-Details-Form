454e23114918f14ca0fc8ba66d90c90212906de9
Victória Nogueira Marques <victoriamarques0604@gmail.com>
2023-08-28 22:08:39 -0300

    Updates

--- index.js
+++ index.js
@@ -5,19 +5,6 @@ function updateCardholderName() {
   nameElement.textContent = input.value;
 }
 
-function updatecardNumber() {
-  const input = document.getElementById("cardNumber");
-  const nameElement = document.querySelector("#frontCard #cardNumbers");
-
-  const maxCharacters = 19;
-
-  if (input.value.length > maxCharacters) {
-    input.value = input.value.slice(0, maxCharacters);
-  }
-
-  nameElement.textContent = input.value;
-}
-
 function updateMonth() {
   const input = document.getElementById("month");
   const nameElement = document.querySelector("#frontCard #nameAndDate h2");
@@ -37,3 +24,32 @@ function updateCvc() {
 
   nameElement.textContent = input.value;
 }
+
+function updateAndValidateCardNumber() {
+  var numberCardInput = document.getElementById("cardNumber");
+  var numberCardErrorMessage = document.getElementById("numberCard-error");
+  var notifyButton = document.getElementById("notify-button");
+  var nameElement = document.querySelector("#frontCard #cardNumbers");
+  var numberCard = numberCardInput.value;
+  var numberCardRegex = /^\d{1,19}$/; // Expressão regular para até 19 dígitos
+
+  const maxCharacters = 19;
+
+  if (numberCard.length > maxCharacters) {
+    numberCardInput.value = numberCard.slice(0, maxCharacters);
+  }
+
+  nameElement.textContent = numberCard;
+
+  if (!numberCardRegex.test(numberCard)) {
+    numberCardInput.style.borderColor = "#ff5050";
+    numberCardErrorMessage.style.display = "block";
+    numberCardErrorMessage.textContent = "Wrong format, numbers only";
+    notifyButton.disabled = true;
+  } else {
+    numberCardInput.style.borderColor = "";
+    numberCardErrorMessage.style.display = "none";
+    notifyButton.disabled = false;
+  }
+}
+
